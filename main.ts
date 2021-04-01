2186a4ad90bf4341e397e8655ec1f77d4cb774fa
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-04-01 18:57:01 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -27,13 +27,15 @@ input.onButtonPressed(Button.B, function () {
         Throttle += -1
     }
 })
+let Yaw = 0
 let Pitch = 0
 let Roll = 0
 let Arm = 0
 let Throttle = 0
-let radioGroup = 7
+let radioGroup = 1
 radio.setGroup(radioGroup)
 basic.showNumber(radioGroup)
+pins.analogWritePin(AnalogPin.P1, 1023)
 basic.forever(function () {
     Roll = input.rotation(Rotation.Roll)
     Pitch = input.rotation(Rotation.Pitch)
@@ -41,10 +43,21 @@ basic.forever(function () {
     if (Arm == 1) {
         led.plot(0, 0)
     }
-    led.plot(0, Math.map(Throttle, 0, 100, 4, 0))
-    led.plot(Math.map(Roll, -45, 45, 0, 4), Math.map(Pitch, -45, 45, 0, 4))
+    if (pins.analogReadPin(AnalogPin.P0) > 500) {
+        Yaw = -30
+        led.plot(1, 4)
+    } else if (pins.analogReadPin(AnalogPin.P2) > 500) {
+        Yaw = 30
+        led.plot(3, 4)
+    } else {
+        Yaw = 0
+        led.plot(2, 4)
+    }
+    led.plot(0, 4 - Throttle / 25)
+    led.plot((45 - Roll) / 22.5, (45 + Pitch) / 22.5)
     radio.sendValue("P", Pitch)
     radio.sendValue("A", Arm)
     radio.sendValue("R", Roll)
     radio.sendValue("T", Throttle)
+    radio.sendValue("Y", Yaw)
 })
